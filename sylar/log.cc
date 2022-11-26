8cbf9a02d1bef89ec2c065340edad12b2378ad82
Yap Miracle <2049829115@qq.com>
2022-11-26 20:39:07 +0800

    add Logger

--- sylar/log.cc
+++ sylar/log.cc
@@ -1,1 +1,45 @@
+#include "log.h"
 
+namespace YAP{
+
+Logger::Logger(const std::string &name)
+    :m_name(name){
+    
+}
+void Logger::addAppender(LogAppender::ptr appender){
+    m_appenders.push_back(appender);
+}
+void Logger::delAppender(LogAppender::ptr appender){
+    for(auto it=m_appenders.begin();
+        it!=m_appenders.end(); it++){
+        if(*it==appender){
+            m_appenders.erase(it);
+            break;
+        }
+    }
+}
+
+void Logger::log(LogLevel::Level level, LogEvent::ptr event){
+    if(level < m_level){
+        return ;
+    }
+    for(auto& i: m_appenders){
+        i->log(level, event);
+    }
+}
+void Logger::debug(LogEvent::ptr event){
+    debug(LogLevel::DEBUG);
+}
+void Logger::info(LogEvent::ptr event){
+    debug(LogLevel::INFO);
+}
+void Logger::warn(LogEvent::ptr event){
+    debug(LogLevel::WARN);
+}
+void Logger::error(LogEvent::ptr event){
+    debug(LogLevel::ERROR);
+}
+void Logger::fatal(LogEvent::ptr event){
+    debug(LogLevel::FATAL);
+}
+}
--- sylar/log.h
+++ sylar/log.h
@@ -1,76 +1,84 @@
-#ifndef __SYLAR_LOG_H
-#define __SYLAR_LOG_H
+#ifndef __YAP_LOG_H
+#define __YAP_LOG_H
 
 #include <iostream>
 #include <stdint.h>
 #include <memory>
+#include <list>
 
-namespace sylar
-{
-    //日志事件
-    class LogEvent
+namespace YAP {
+//日志事件
+class LogEvent {
+public:
+    typedef std::shared_ptr<LogEvent> ptr;
+    LogEvent();
+
+private:
+    const char *m_file = nullptr; //文件名
+    int32_t m_line = 0;           //行号
+    uint32_t m_elapse = 0;        //程序启动开始到现在的毫秒数
+    uint32_t m_threadId = 0;      //线程id
+    uint32_t m_fiberId = 0;       //协程id
+    uint64_t m_time = 0;          //事件戳
+    std::string m_content;
+};
+
+//日志级别
+class LogLevel {
+public:
+    enum Level
     {
-    public:
-        typedef std::shared_ptr<LogEvent> ptr;
-        LogEvent();
-
-    private:
-        const char *m_file = nullptr; //文件名
-        int32_t m_line = 0;           //行号
-        uint32_t m_elapse = 0;        //程序启动开始到现在的毫秒数
-        uint32_t m_threadId = 0;      //线程id
-        uint32_t m_fiberId = 0;       //协程id
-        uint64_t m_time = 0;          //事件戳
-        std::string m_content;
+        DEBUG = 1,
+        INFO = 2,
+        WARN = 3,
+        ERROR = 4,
+        FATAL = 5
     };
+};
 
-    //日志级别
-    class LogLevel
+// 日志格式器
+class LogFormatter {
+public:
+    typedef std::shared_ptr<LogFormatter> ptr;
+    std::string format(LogEvent::ptr event);
+
+private:
+};
+
+//日志输出地
+class LogAppender {
+public:
+    typedef std::shared_ptr<LogAppender> ptr;
+    virtual ~LogAppender() {}
+    void log(LogLevel::Level level, LogEvent::ptr event)
     {
-    public:
-        enum Level
-        {
-            DEBUG = 1,
-            INFO = 2,
-            WARN = 3,
-            ERROE = 4,
-            FATAL = 5
-        };
-    };
+    }
 
-    // 日志格式器
-    class LogFormatter
-    {
-    public:
-        typedef std::shared_ptr<LogFormatter> ptr;
-        std::string format(LogEvent::ptr event);
+private:
+};
 
-    private:
-    };
+//日志器
+class Logger {
+public:
+    typedef std::shared_ptr<Logger> ptr;
+    Logger(const std::string &name = "root");
+    void log(LogLevel::Level level, LogEvent::ptr evnet);
 
-    //日志输出地
-    class LogAppender
-    {
-    public:
-        typedef std::shared_ptr<LogAppender> ptr;
-        virtual ~LogAppender() {}
-        void log(LogLevel::Level level, LogEvent::ptr evnet)
-        {
-        }
+    void debug(LogEvent::ptr event);
+    void info(LogEvent::ptr event);
+    void warn(LogEvent::ptr event);
+    void error(LogEvent::ptr event);
+    void fatal(LogEvent::ptr event);
 
-    private:
-    };
-
-    //日志器
-    class Logger
-    {
-    public:
-        typedef std::shared_ptr<Logger> ptr;
-        Logger(const std::string &name = "root");
-        void log(LogLevel::Level level, LogEvent::ptr evnet);
-
-    private:
-    };
-}
+    void addAppender(LogAppender::ptr appender);
+    void delAppender(LogAppender::ptr appender);
+    LogLevel::Level getLevel() const {return m_level;}
+    void setLevel(LogLevel::Level _level) {m_level = _level;}
+private:
+    std::string m_name;                     // name
+    LogLevel::Level m_level;                // level
+    std::list<LogAppender::ptr> m_appenders;// appenders collections
+};
+} // namesapce YAP
 
 #endif
